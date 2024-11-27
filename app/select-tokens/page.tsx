036077baffcddfe5bc6c21d1ab44584bce31ffd1
Oshioke Salaki <salaki1902@gmail.com>
2024-11-27 20:40:24 +0100

    fix: page.tsx

--- app/select-tokens/page.tsx
+++ app/select-tokens/page.tsx
@@ -3,7 +3,7 @@ import Selecttokens from "../components/select-tokens";
 
 const page = () => {
   return (
-    <div className="bg-main-bg bg-center md:bg-left w-full  bg-cover bg-no-repeat">
+    <div className="bg-main-bg bg-center md:bg-left w-full  bg-cover bg-no-repeat sm:h-[100vh] h-[150vh]">
       <Selecttokens />;
     </div>
   );
