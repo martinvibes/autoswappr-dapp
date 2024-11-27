b671bed4a2ea06f028a4a87fb30f0a334f89e17c
Oshioke Salaki <salaki1902@gmail.com>
2024-11-27 20:41:06 +0100

    fix: select-tokens.tsx

--- app/components/select-tokens.tsx
+++ app/components/select-tokens.tsx
@@ -69,19 +69,16 @@ const Selecttokens = () => {
   return (
     <div className=" w-full pt-[180px] overflow-x-hidden items-center justify-center flex">
       <div
-        className={`shadow-lg rounded-3xl w-[350px] border-[#170F2E] border-2 sm:w-[800px] ${openSans.className}  h-[660px] flex justify-center flex-col items-center`}
+        className={`shadow-lg relative rounded-3xl w-[350px] border-[#170F2E] border-2 sm:w-[800px] ${openSans.className} p-6 flex justify-center flex-col items-center`}
       >
-        <div className=" w-[80%] flex items-center justify-between">
-          <p className="none sm:block"></p>
-          <p className="font-[600]  text-center sm:text-left text-[20px] sm:text-[24px] ">
-            Select token(s) to auto-swap from
-          </p>
-          <div className="hidden md:block">
-            <CloseButton />
-          </div>
-        </div>
-        <br />
-        <p className="text-center font-normal text-[14px] sm:text-[16px]  leading-[22px] w-[318px] sm:w-[620px] ">
+        <button className="hidden md:block absolute top-4 right-4 z-10">
+          <CloseButton />
+        </button>
+        <p className="font-[600]  text-center sm:text-left text-lg sm:text-[24px] mb-4">
+          Select token(s) to auto-swap from
+        </p>
+
+        <p className="text-center font-normal text-[12px] sm:text-[16px]  leading-[22px]">
           You can select multiple tokens to auto-swap from here. You can select
           how many percent of that token should be auto-swapped whenever you get
           funded.
@@ -89,10 +86,10 @@ const Selecttokens = () => {
         <br />
 
         <form className="mx-auto flex items-center justify-center flex-col">
-          <div className="relative bg-[#100827] px-4 py-2 flex items-center justify-between w-[318px] sm:w-[704px] rounded-full">
+          <div className="relative bg-[#100827] px-4 py-2 flex items-center justify-between w-full rounded-full">
             <input
               type="search"
-              className="w-[600px] bg-[#100827] text-[14px] p-1 text-white border-none focus:outline-none focus:border-none focus:no-underline  "
+              className=" bg-[#100827] text-[14px] p-1 text-white border-none focus:outline-none focus:border-none focus:no-underline  "
               placeholder="Search tokens..."
               value={searchTerm}
               onChange={(e) => setSearchTerm(e.target.value)}
@@ -100,18 +97,17 @@ const Selecttokens = () => {
             />
             <SearchIcon />
           </div>
-          <br />
-          <div className="flex  w-[318px] sm:w-[704px] flex-col">
-            <div className="flex  items-center  gap-2">
+          <div className="flex flex-col mt-4">
+            <div className="flex  items-center md:text-sm text-xs  gap-2">
               <TimeIcon />
               <p>Selected tokens</p>
             </div>
-            <div className="grid mt-4 grid-cols-2 sm:grid-cols-3 gap-1 sm:gap-4 ">
+            <div className="grid mt-4 grid-cols-2 sm:grid-cols-3 gap-3 sm:gap-4 ">
               {filteredCoins.map((coin) => (
                 <div
                   key={coin.key}
                   onClick={() => toggleCoinSelection(coin.key)}
-                  className={`sm:w-[224px] w-[150px] h-[48px] rounded-full border-[#170F2E] py-1 px-2 grid grid-cols-[24px_auto_30px] sm:grid-cols-[35px_auto_30px] cursor-pointer 
+                  className={`sm:w-[224px] w-[150px] h-[60px] md:h-[60px] rounded-full border-[#170F2E] py-1 px-2 grid grid-cols-[24px_auto_30px] items-center sm:grid-cols-[35px_auto_30px] cursor-pointer 
                     ${selectedCoins.includes(coin.key) ? "bg-[#0F96E3]/10" : ""}`}
                 >
                   <div className="items-center justify-center flex">
@@ -123,11 +119,11 @@ const Selecttokens = () => {
                       className="sm:w-8 sm:h-8 "
                     />
                   </div>
-                  <div className="flex pl-2 flex-col items-start justify-between text-center">
-                    <p className="text-[14px] sm:text-[16px] font-normal sm:font-semibold">
+                  <div className="flex pl-2 flex-col items-start">
+                    <p className="text-[12px] sm:text-[16px] font-normal sm:font-semibold">
                       {coin.coinName}
                     </p>
-                    <p className="capitalize text-[12px] sm:text-[13px] text-[#433B5A]">
+                    <p className="capitalize text-[10px] sm:text-[13px] text-[#433B5A]">
                       {coin.coinSymbol}
                     </p>
                   </div>
@@ -143,11 +139,8 @@ const Selecttokens = () => {
             </div>
           </div>
         </form>
-        <br />
-        <hr className="border-[#100827] w-[704px]" />
-        <br />
         <button
-          className="w-[318px] sm:w-[704px] text-white bg-[#100827] h-[60px] rounded-[32px]"
+          className="w-full text-white bg-[#100827] h-[60px] rounded-[32px] mt-10"
           disabled={selectedCoins.length === 0}
         >
           Next
