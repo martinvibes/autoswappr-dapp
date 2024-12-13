dc5c9af0ebbf24f4bda4529be38320bc22e26fb6
abdegenius <johnnieabijah2000@gmail.com>
2024-12-13 18:38:08 +0100

    refactor: added date/time to mobile screen

--- app/components/activity-log.tsx
+++ app/components/activity-log.tsx
@@ -183,7 +183,9 @@ const ActivityLog = () => {
                         />
                       </div>
                       <div className="capitalize text-main-white">
-                        <h2 className="text-sm md:text-base">{data.from.coinFrom}</h2>
+                        <h2 className="text-sm md:text-base">
+                          {data.from.coinFrom}
+                        </h2>
                         <h3 className="text-[#433B5A] text-xs md:text-sm">
                           {data.from.coinTo}
                         </h3>
@@ -199,7 +201,9 @@ const ActivityLog = () => {
                     </h3>
                   </td>
                   <td className="bg-[#100827] rounded-full w-fit h-fit place-self-center px-6 py-3">
-                    <span className="text-xs md:text-sm">{data.percentage}%</span>
+                    <span className="text-xs md:text-sm">
+                      {data.percentage}%
+                    </span>
                   </td>
                   <td className="uppercase text-main-white place-self-end">
                     <h2 className="text-base">{data.date.day}</h2>
@@ -211,30 +215,36 @@ const ActivityLog = () => {
           </tbody>
         </table>
         {/* mobile */}
-        <table className="sm:hidden leading-[19.07px] max-h-[400px] overflow-auto scrollbar-hide w-full max-w-[94%] mx-auto">
-          <thead>
-            <tr className="capitalize text-main-white flex justify-between border-b border-[#433B5A] pb-3">
-              <th>token</th>
-              <th>amount</th>
+        <table className="sm:hidden leading-[18px] w-full mx-auto">
+          <tbody className="capitalize text-main-white w-full flex flex-col flex-none justify-between h-full max-h-[550px] overflow-auto scrollbar-hide px-2">
+            <tr className="w-full capitalize text-main-white flex flex-none justify-between text-start border-b border-[#433B5A] pb-3 space-x-4">
+              <th className="flex-none flex items-center justify-start w-[50%]">
+                token
+              </th>
+              <th className="flex-none flex items-center justify-start w-[40%]">
+                amount
+              </th>
+              <th className="flex-none flex items-center justify-end w-auto">date/time</th>
             </tr>
-          </thead>
-          <tbody className="capitalize text-main-white flex flex-col justify-between">
             {activityLog.map((data, index) => {
               return (
-                <tr className="py-5 flex justify-between w-full" key={index}>
-                  <td className="flex items-center gap-[12px]">
-                    <h1 className="text-[#433B5A]">{index + 1}</h1>
-                    <div className="flex gap-10 items-center">
-                      <div className="flex relative">
+                <tr
+                  className="py-5 flex flex-none justify-between w-full space-x-4"
+                  key={index}
+                >
+                  <td className="w-[50%] flex flex-row flex-none items-center gap-2">
+                    <h1 className="text-[#433B5A]">{index + 1}. </h1>
+                    <div className="flex gap-8 items-center">
+                      <div className="flex flex-none relative">
                         <Image
                           src={data.from.fromImage}
                           alt="coin from"
-                          className="w-6 h-6"
+                          className="w-6 h-6 flex-none"
                         />
                         <Image
                           src={data.from.toImage}
                           alt="coin to"
-                          className="absolute left-5"
+                          className="absolute left-5 flex-none"
                         />
                       </div>
                       <div className="capitalize text-main-white">
@@ -245,13 +255,17 @@ const ActivityLog = () => {
                       </div>
                     </div>
                   </td>
-                  <td className="uppercase text-main-white text-start">
+                  <td className="w-[40%] uppercase text-main-white text-start flex flex-col flex-none">
                     <h2 className="font-normal text-sm text-start">
                       {data.to.coinFromAmount} {data.to.coinFrom}
                     </h2>
                     <h3 className="text-[#433B5A] font-semibold text-xs text-start">
                       {data.to.coinToAmount} {data.to.coinTo}
                     </h3>
+                  </td>
+                  <td className="w-fit uppercase text-main-white place-self-end flex flex-col flex-none">
+                    <h2 className="text-base">{data.date.day}</h2>
+                    <h3 className="text-[#433B5A] text-sm">{data.date.time}</h3>
                   </td>
                 </tr>
               );
